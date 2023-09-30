6cbff3dfe4b45c5d8bf676c4a5ad18ee7ca88865
root <root@ip-172-31-18-131.ap-southeast-1.compute.internal>
2023-09-30 09:11:20 +0000

    main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+        region = "ap-southeast-1"
+}
+
+terraform {
+        backend "s3" {
+               bucket = "tf-test1-bucket"
+               key = "terrafrom.tfstate"
+               region = "ap-southeast-1"
+        }
+}
+
+resource "aws_instance" "tf_new_inst" {
+        subnet_id = "subnet-02baa8d1f25c28d2d"
+        ami_id = "ami-0df7a207adb9748c7"
+        instance_type = "t2.micro"
+}
