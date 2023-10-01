b7dec7c5073ef1ca8ee8a15825c0e0cb7ca1d12f
user1 <user1@mail.com>
2023-10-01 06:54:19 +0000

    main.tf modf

--- main.tf
+++ main.tf
@@ -14,4 +14,8 @@ resource "aws_instance" "tf_new_inst" {
         subnet_id = "subnet-02baa8d1f25c28d2d"
         ami = "ami-0df7a207adb9748c7"
         instance_type = "t2.micro"
+        tags = {
+               Name = "tf_new_inst"
+               Env = "new_dev_inst"
+        }
 }
